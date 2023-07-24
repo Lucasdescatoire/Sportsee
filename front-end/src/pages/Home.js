4c186cc64cffe63862d9406cfd47fccc39db7c1a
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2023-07-24 08:20:23 +0200

    fin page home , ajout card page home

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Card from "../components/Card";
+import Man from "../assets/users/man.png";
+import Woman from "../assets/users/woman.png";
 
 const Home = () => {
   return (
-    <main className="container">
-      <h1>Bienvenue sur SportSee</h1>
+    <main className="container home">
+      <h1>
+        Bienvenue sur <span>SportSee</span>
+      </h1>
       <p>Une application mobile dédiée au coaching sportif</p>
       <div className="users">
-        <NavLink to="user/12">👦 Karl </NavLink>
-        <NavLink to="user/18">👩 Cecilia </NavLink>
+        <NavLink to="user/12">
+          <Card icon={Man} info="Karl" text="Consulter le profil" />
+        </NavLink>
+        <NavLink to="user/18">
+          <Card icon={Woman} info="Cécilia" text="Consulter le profil" />
+        </NavLink>
       </div>
     </main>
   );
