31a18232143e639226686e269e7a9cc0514d5b10
Lucasdescatoire <72078978+Lucasdescatoire@users.noreply.github.com>
2023-07-24 08:24:48 +0200

    page 404  ,card page dashboard ,fichier charts

--- front-end/src/pages/Dashboard.js
+++ front-end/src/pages/Dashboard.js
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import WelcomeUser from "../components/WelcomeUser";
 import { getData } from "../data/services/getdata";
 import Error from "./Error";
+import WelcomeUser from "../components/WelcomeUser";
+import Card from "../components/Card";
+// Icon
+import iconCalories from "../assets/icons-nutrition/calories.svg";
+import iconGlucides from "../assets/icons-nutrition/glucides.svg";
+import iconLipides from "../assets/icons-nutrition/lipides.svg";
+import iconProteines from "../assets/icons-nutrition/proteines.svg";
 
 const Dashboard = () => {
   const { id } = useParams();
@@ -11,7 +17,7 @@ const Dashboard = () => {
   useEffect(() => {
     const data = async () => {
       const request = await getData("USER_MAIN_DATA", id);
-      if (!request) return alert("data error");
+      if (!request) return <Error />;
       setData(request.data);
     };
     data();
@@ -20,8 +26,37 @@ const Dashboard = () => {
   if (data.length === 0) return <Error />;
 
   return (
-    <main className="container">
+    <main className="container dashboard">
       <WelcomeUser name={data.userInfos.firstName} />
+      <section className="statistique">
+        <div className="charts"></div>
+        <div className="keydata">
+          <Card
+            icon={iconCalories}
+            info={data.keyData.calorieCount}
+            unit="kCal"
+            text="Calories"
+          />
+          <Card
+            icon={iconProteines}
+            info={data.keyData.proteinCount}
+            unit="g"
+            text="Proteines"
+          />
+          <Card
+            icon={iconGlucides}
+            info={data.keyData.carbohydrateCount}
+            unit="g"
+            text="Glucides"
+          />
+          <Card
+            icon={iconLipides}
+            info={data.keyData.lipidCount}
+            unit="g"
+            text="Lipides"
+          />
+        </div>
+      </section>
     </main>
   );
 };
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
       <h1>
         Bienvenue sur <span>SportSee</span>
       </h1>
-      <p>Une application mobile dédiée au coaching sportif</p>
+      <p>Une application mobile dédiée au coaching sportif !</p>
       <div className="users">
         <NavLink to="user/12">
           <Card icon={Man} info="Karl" text="Consulter le profil" />
